9ba520589469fbf469cab5a5746eda155694d73f
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-02-27 19:47:33 -0800

    modificacion 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tarea_grupa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
